0ff3f7297f43c15c01deeccc6a78af38328d88bd
Joel Myers <myersoftsolutions@gmail.com>
2017-05-20 15:41:26 -0400

    Home UI Build out

--- Legends TV/Legends TV/LTVHomeViewController.swift
+++ Legends TV/Legends TV/LTVHomeViewController.swift
@@ -14,19 +14,36 @@ class LTVHomeTableCell : UITableViewCell {
     @IBOutlet var imgBottomCon: NSLayoutConstraint!
 }
 
-class LTVHomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource/* UICollectionViewDataSource, UICollectionViewDelegate*/ {
+class LTSavedVidCollectionCell : UICollectionViewCell {
+
+}
+
+class LTVHomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UICollectionViewDataSource, UICollectionViewDelegate {
     @IBOutlet var savedVidsViewHeightCon: NSLayoutConstraint!
 
+    @IBOutlet var savedVidBtnHeightCon: NSLayoutConstraint!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+//        self.savedVidBtnHeightCon.constant = 0
+//        self.savedVidsViewHeightCon.constant = 0
 
-        // Do any additional setup after loading the view.
+                // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+
+        
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    //MARK: UITableView Datasource Methods
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 5
@@ -37,6 +54,18 @@ class LTVHomeViewController: UIViewController, UITableViewDelegate, UITableViewD
         
         return cell
     }
+    
+    //MARK: UICollectionView Datasource Methods
 
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "SavedVidCell", for: indexPath) as! LTSavedVidCollectionCell
+        
+        return cell
+    }
 
 }
