9d549c6b7d3792d3335f8fbd1455d7a1cb7dc53c
Joel Myers <myersoftsolutions@gmail.com>
2017-05-27 16:35:39 -0400

    LTViewingView custom TableCells and swift files added

---
+++ Legends TV/Legends TV/LTVDescriptionTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  LTVDescriptionTableViewCell.swift
+//  Legends TV
+//
+//  Created by Joel Myers on 5/27/17.
+//  Copyright © 2017 Legends Media. All rights reserved.
+//
+
+import UIKit
+
+class LTVDescriptionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Legends TV/Legends TV/LTVViewingViewController.swift
+++ Legends TV/Legends TV/LTVViewingViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class LTVViewingViewController: UIViewController {
+class LTVViewingViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,16 +20,7 @@ class LTVViewingViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
+
+
