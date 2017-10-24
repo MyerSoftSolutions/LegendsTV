b61124dd09322fe1802df64cadcfca01576e3f31
Joel Myers <myersoftsolutions@gmail.com>
2017-10-24 18:16:23 -0400

    Refactored Home and DetailViewControllers added to Storyboard and file set

---
+++ Legends TV/Legends TV/LegendsDetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  LegendsDetailViewController.swift
+//  Legends TV
+//
+//  Created by Joel Myers on 10/24/17.
+//  Copyright © 2017 Legends Media. All rights reserved.
+//
+
+import UIKit
+
+class LegendsDetailViewController: UIViewController {
+    @IBOutlet weak var slideUpBtn: UIButton!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descTextView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func slideUpTapped(_ sender: Any) {
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Legends TV/Legends TV/LegendsHomeViewController.swift
@@ -0,0 +1,31 @@
+//
+//  LegendsHomeViewController.swift
+//  Legends TV
+//
+//  Created by Joel Myers on 10/24/17.
+//  Copyright © 2017 Legends Media. All rights reserved.
+//
+
+import UIKit
+
+class LegendsHomeViewController: UIViewController, UIScrollViewDelegate {
+
+    @IBOutlet weak var homeScrollView: UIScrollView!
+    @IBOutlet weak var homePageControl: UIPageControl!
+    @IBOutlet weak var slideUpBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    
+
+}
